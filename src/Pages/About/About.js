8dea7787100d881a790f267e8b9013584e71e109
besimsherifi <sherifibesim@gmail.com>
2025-10-15 14:45:40 +0200

    added some more navigation on the home page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,7 +3,11 @@ import CoreValues from "../About/CoreValues";
 import HowWeWork from "../About/HowWeWork";
 import ServicesGrid from "../About/ServicesGrid";
 import Banner from "../../Components/Banner";
+import { useNavigate } from "react-router-dom";
+
 export default function About() {
+    const navigate = useNavigate();
+
     return (
         <>
             <ServicesBanner />
@@ -15,7 +19,7 @@ export default function About() {
                 titlePrimary="Ready for a"
                 titleEmphasis="Spotless Space?"
                 ctaText="Book a Visit Now"
-                onCtaClick={() => console.log('Book clicked')}
+                onCtaClick={() => navigate("/contact-us")}
                 imageSrc={require("../../assets/banner-image-min.png")}
                 imageAlt="Cleaner with gloves"
             />
